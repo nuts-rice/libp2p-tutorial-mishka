42d588032195f530d146daa01b9015c235c8bcb9
nuts-rice <red44443@gmail.com>
2022-01-15 23:05:27 -0500

    need to give explicit type i guess?

--- rust-libp2p-tutorial/src/main.rs
+++ rust-libp2p-tutorial/src/main.rs
@@ -1,12 +1,74 @@
+use libp2p::{
+    core::upgrade,
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    futures::StreamExt,
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{NetworkBehaviourEventProcess, Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    NetworkBehaviour, PeerId, Transport,
+};
 
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
 
 const STORAGE_FILE_PATH: &str = "./recipes.json";
-type Result<T> = std::result::Result<T, Box<dyn std::Error + Send + Sync + 'static>>;
-static KEYS: Lazy<identify::Keypair> = Lazy::new(|| identify::Keypair::generate_ed25519());
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+type Recipes = Vec<Recipe>;
+
+static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
 static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
 static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Recipe {
+    id: usize,
+    name: String,
+    ingredients: String,
+    instructions: String,
+    public: bool,
+}
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Serialize, Deserialize)]
+enum ListMode {
+    ALL,
+    One(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListRequest {
+    mode: ListMode,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ListResponse {
+    mode: ListMode,
+    data: Recipes,
+    reciever: String,
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer Id: {}", PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("can create auth keys");
+
+    //Handhake Noise secure comms and make nice nice!
+    //xx is interoperabality pilled
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
 }
